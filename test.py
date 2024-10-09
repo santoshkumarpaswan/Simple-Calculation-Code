ad8a29ae94556286d26cfe4468905bd16ebb296b
santosh <santosh@gmail.com>
2024-10-09 17:37:15 +0530

    added some lines

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
 #This CODE Refers to Simple Calculator like Adding, Sub, Multiply 
 
 num1 =  float(input("Please Enter The First Number Here"))
+num2 =  float(input("Please Enetr The Second Number Here"))
+
+#Perform Opertaions
+print(f"Sum: {num1 + num2}")
+print(f"sub: {num1 + num2}")
+print(f"product: {num1 + num2}")
+print(f"Quotient: {num1 + num2}")
